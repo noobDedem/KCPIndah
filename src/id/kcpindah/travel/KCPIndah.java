3fc7c8e36d1317dcd10a7dbbf97b0bec1d75cfe2
doublesinlove <rizqihadiprawira@gmail.com>
2017-06-11 22:57:39 +0700

    Add logo. (Please make sure to change Connection settings)

--- src/id/kcpindah/travel/KCPIndah.java
+++ src/id/kcpindah/travel/KCPIndah.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class KCPIndah extends Application {
@@ -13,6 +14,7 @@ public class KCPIndah extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("view/SignInForm.fxml"));
         primaryStage.setTitle("KCP Indah");
         primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("TravelLogo.png")));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
